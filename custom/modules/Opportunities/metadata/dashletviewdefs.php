0bf90728ec3e54880131daa28c97da959f24e3ad
Timothy Greeninger <tjgreeninger@gmail.com>
2016-03-07 21:18:17 -0800

    Fixes issue #1

--- custom/modules/Opportunities/metadata/dashletviewdefs.php
+++ custom/modules/Opportunities/metadata/dashletviewdefs.php
@@ -10,23 +10,15 @@ $dashletData['OpportunitiesDashlet']['searchFields'] = array (
   ),
   'sales_stage' => 
   array (
-    'default' => 
-    array (
-      0 => 'Prospecting',
-      1 => 'Qualification',
-      2 => 'Needs Analysis',
-      3 => 'Value Proposition',
-      4 => 'Id. Decision Makers',
-      5 => 'Perception Analysis',
-      6 => 'Proposal/Price Quote',
-      7 => 'Negotiation/Review',
-    ),
+    'default' => '',
   ),
   'assigned_user_id' => 
   array (
-    'type' => 'assigned_user_name',
-    'label' => 'LBL_ASSIGNED_TO',
-    'default' => 'Administrator',
+    'default' => '',
+  ),
+  'date_modified' => 
+  array (
+    'default' => '',
   ),
 );
 $dashletData['OpportunitiesDashlet']['columns'] = array (
