05df9a03616f10f38a638537b85dea96d77b7e42
Timothy Greeninger <tjgreeninger@gmail.com>
2016-05-19 09:24:55 -0700

    Second Commit

--- config_override.php
+++ config_override.php
@@ -3,4 +3,10 @@
 $sugar_config['default_date_format'] = 'm/d/Y';
 $sugar_config['default_time_format'] = 'H:i';
 $sugar_config['disable_persistent_connections'] = false;
+$sugar_config['logger']['level'] = 'debug';
+$sugar_config['default_module_favicon'] = false;
+$sugar_config['dashlet_auto_refresh_min'] = '30';
+$sugar_config['enable_action_menu'] = true;
+$sugar_config['stack_trace_errors'] = false;
+$sugar_config['developerMode'] = false;
 /***CONFIGURATOR***/
